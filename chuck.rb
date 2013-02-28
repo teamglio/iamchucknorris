401b557764a32e49c8ee7a3716f8a9df8caf6455
Emile Silvis <emile@silvis.co.za>
2013-02-28 15:52:05 +0200

    Mixup referral ID

--- chuck.rb
+++ chuck.rb
@@ -4,6 +4,6 @@ require 'json'
 
 get '/' do
 	@joke = JSON.load(RestClient.get 'http://api.icndb.com/jokes/random?exclude=[explicit,nerdy]')["value"]["joke"]
-	@mixup_ad = RestClient.get 'http://serve.mixup.hapnic.com/8215822'
+	@mixup_ad = RestClient.get 'http://serve.mixup.hapnic.com/9392697'
 	erb :joke
 end
